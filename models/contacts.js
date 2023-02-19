37c7d24ea58bff4126e2d8d13f0b1ad9478bcff7
PavloPSh <pavlopshe@gmail.com>
2023-02-20 01:28:23 +0200

    update

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,43 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require('path');
 
-const listContacts = async () => {}
+// const { nanoid } = require('nanoid');
+const { v4: uuidv4 } = require('uuid');
 
-const getContactById = async (contactId) => {}
+const contactsPath = path.resolve('./models/contacts.json');
 
-const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const result = contacts.find(contact => contact.id === id);
+    if (!result) {
+        return null;
+    };
+  return result;
+}
+
+const addContact = async (body) => {
+
+  const contacts = await listContacts();
+  const newContacts = { ...body, id: uuidv4() };
+  contacts.push(newContacts);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContacts;
+
+};
+
+
+
+const removeContact = async (id) => {}
+
+
 
 const updateContact = async (contactId, body) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,55 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
+
+
+const contactsOperations = require('../../models/contacts');
+
+const HttpError = require('../../helpers')
 
 const router = express.Router()
 
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'Homework 2' })
+  try {
+    const result = await contactsOperations.listContacts();
+    res.status(200).json(result);
+  } catch (error) {
+      next(error);
+
+  };
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'Homework 2' })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsOperations.getContactById(id);
+    if (!result) {
+      throw HttpError(404, 'Not Found');
+    }
+    res.status(200).json(result)
+  } catch (error) {
+      next(error);
+  };
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'Homework 2' })
+  try {
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      throw HttpError(400, error.message);
+    };
+    const result = await contactsOperations.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+      next(error);
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
